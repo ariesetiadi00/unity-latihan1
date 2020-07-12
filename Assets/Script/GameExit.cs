1b6d8fb04a06d0c3ba0f5825738d4a7f265c0c7a
ariesetiadi00 <ariesetiadi00@gmail.com>
2020-07-12 13:26:51 +0800

    Menambah start dan credits scene

---
+++ Assets/Script/GameExit.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GameExit : MonoBehaviour {
+    public void gameExit(){
+        Debug.Log("System Exit");
+        Application.Quit();
+    }    
+}
---
+++ Assets/Script/GameStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    public void gameStart(){
+        Debug.Log("Game Start");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
